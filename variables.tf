a867463051e0932e559f55873b48ed4c295bf3cb
Mark Wragg <mark.wragg@xe.com>
2018-10-29 00:44:50 +0000

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,24 @@
+#AWS
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {}
+
+variable "grafana_port" {
+  default = 8080
+}
+
+variable "influx_port" {
+  default = 8086
+}
+
+variable "influx_database" {
+  default = "metrics"
+}
